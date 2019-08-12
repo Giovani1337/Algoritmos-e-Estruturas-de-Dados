fd961fec882f04341e255080a5e75f443b96e59f
Giovani1337 <36922813+Giovani1337@users.noreply.github.com>
2019-08-12 19:28:42 -0300

    Update Ex 3 - Lista 1.c

--- Ex 3 - Lista 1.c
+++ Ex 3 - Lista 1.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+/*A Série de Fibonacci é uma série matemática deﬁnida da seguinte forma: Os dois primeiros termos da série são o número 1,
+do terceiro termo em diante, o termo será o resultado da soma dos dois termos imediatamente anteriores.
+Abaixo, temos os oito primeiros termos da série:
+1 1 2 3 5 8 13 21 ...
+Escreva uma função recursiva que retorne o enésimo termo da série de ﬁbonacci. */
 int fibonacci( int termo )
 {
     if (termo == 1 || termo == 2 )
